24ba2979c9be841ffb031603109accba4326c83c
luis-miro <96116501+luis-miro@users.noreply.github.com>
2024-01-28 19:09:34 +0100

    Update variables.js

--- webassets/variables.js
+++ webassets/variables.js
@@ -6,7 +6,7 @@ var S3_RECORD_TERMS_ACCEPTANCE_ENDPOINT_URL = 'replace with the value of S3Recor
 
 var S3_BUCKET_BASE_URL = 'replace with the value of S3BucketBaseUrl from your AWS SAM CLI Output'; // keep the single quotes around the value (e.g. 'https://miro-terms-banner-021ba44a2fc7.s3.eu-central-1.amazonaws.com';)
 
-var SECONDS_TO_WAIT = 1; // Enter value as number (not string) - Enter value as number (not string) - This is the interval in seconds the app will check if the current user has accepted the terms modal. With the default value (3) the app will check every 3 seconds if the user has accepted the modal until the terms modal has been successfully accepted
+var SECONDS_TO_WAIT = 3; // Enter value as number (not string) - Enter value as number (not string) - This is the interval in seconds the app will check if the current user has accepted the terms modal. With the default value (3) the app will check every 3 seconds if the user has accepted the modal until the terms modal has been successfully accepted
 
 var MODAL_WITH = 590; // Enter value as number (not string) - Width of the modal pop-up in pixels - Adjust as desired
 
